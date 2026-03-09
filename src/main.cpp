c667c9f0195251815e18762fd3b9e4cecf1e4c62
BaghinyanArt <artur.baghinyan555gmail.com>
2026-03-09 23:46:57 +0400

    Starting some base work

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include "TaskManager.h"
+
+using namespace std;
+
 
 int main() {
-    std::cout << "Hello, Task Manager!" << std::endl;
-    return 0;
+    int button;
+
+    do {
+        system("cls");
+        cout << "Welcome to task menager\n";
+
+        cin >> button;
+        switch (button) {
+        case 4:
+            cout << "we in case 4\n";
+            int something;
+            cout << "enter some integer: ";
+            cin >> something;
+            break;
+        default:
+            break;
+        }
+    } while (0 != button);
+
+
+
 }
