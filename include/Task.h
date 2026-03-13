61d4cbc33540f4dd7ac075dfec176850e7ad7fc8
BaghinyanArt <artur.baghinyan555gmail.com>
2026-03-14 00:37:53 +0400

    Removing the description part from task

--- include/Task.h
+++ include/Task.h
@@ -23,22 +23,19 @@ class Task {
 public:
 	taskID getID() const { return id; }
 	const std::string& getName() const { return name; }
-	const std::string& getDescription() const { return description; }
 	const Date& getDate() const { return dueDate; }
 	Status getStatus() const { return currentStatus; }
 
 	Task(const std::string& n, const std::string& d, const Date& dd, Status cs) :
-		name(n), description(d), dueDate(dd), currentStatus(cs){
+		name(n), dueDate(dd), currentStatus(cs){
 	}
 	void setName(const std::string& n) { name = n; }
-	void setDescription(const std::string& d) { description = d; }
 	void setDate(const Date& dd) { dueDate = dd; }
 	void setStatus(Status cs) { currentStatus = cs; }
 
 private :
 	taskID id;
 	std::string name;
-	std::string description;
 	Date dueDate;
 	Status currentStatus;
 };
