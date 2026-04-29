31e67eea74a6707271a9cdc3ff3f47307758857e
BaghinyanArt <artur.baghinyan555gmail.com>
2026-04-29 21:51:00 +0400

    small visual changes

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include "TaskManager.h"
 using namespace std;
 
+void clearing();
+
 int main() {
     int button;
     //for using getline
@@ -16,7 +18,7 @@ int main() {
         << "\nFor selecting one of the options enter the corresponding number." << endl;
 
     do {
-        cout << "**********************\n"
+        cout << "********OPTIONS*******\n"
             << "[1] Creating task\n"
             << "[2] Displaying your tasks\n"
             << "[3] Update task\n"
@@ -34,23 +36,29 @@ int main() {
 
         switch (button) {
         case 0: {
+            clearing();
             cout << "Exiting program..." << endl;
             break;
         }
         case 1: {
+            clearing();
             cout << "Enter your task name: ";
             string taskName;
             getline(cin, taskName);
             tableOfTasks.addingTask(taskName);
+            cout << endl;
             break;
         }
         case 2: {
+            clearing();
             if (tableOfTasks.checkForEmptinees()) {
                 tableOfTasks.displayingTasks();
             }
+            cout << endl;
             break;
         }
         case 3: {
+            clearing();
             if (tableOfTasks.checkForEmptinees()) {
                 int inputID;
                 string stringInputID;
@@ -59,9 +67,11 @@ int main() {
                 inputID = stoi(stringInputID);
                 tableOfTasks.updateTask(inputID);
             }
+            cout << endl;
             break;
         }
         case 4: {
+            clearing();
             if (tableOfTasks.checkForEmptinees()) {
                 int inputID;
                 string stringInputID;
@@ -70,9 +80,11 @@ int main() {
                 inputID = stoi(stringInputID);
                 tableOfTasks.markAsCompleted(inputID);
             }
+            cout << endl;
             break;
         }
         case 5: {
+            clearing();
             if (tableOfTasks.checkForEmptinees()) {
                 int inputID;
                 string stringInputID;
@@ -81,9 +93,11 @@ int main() {
                 inputID = stoi(stringInputID);
                 tableOfTasks.removeTask(inputID);
             }
+            cout << endl;
             break;
         }
         case 6: {
+            clearing();
             if (tableOfTasks.checkForEmptinees()) {
                 int inputID;
                 string stringInputID;
@@ -92,21 +106,34 @@ int main() {
                 inputID = stoi(stringInputID);
                 tableOfTasks.saveTaskToFile(inputID);
             }
+            cout << endl;
             break;
         }
+        //need to fix
         case 7: {
+            clearing();
             tableOfTasks.removeFileContent();
+            cout << endl;
             break;
         }
+        //need to fix
         case 8: {
+            clearing();
             tableOfTasks.loadFromFile();
+            cout << endl;
             break;
         }
         default:
-            cout << "Invalid choice. Choose one of the options above!" << endl;
+            clearing();
+            cout << "Invalid choice. Choose one of the following options!" << endl;
+            cout << endl;
             break;
         }
     } while (0 != button);
 
     return 0;
+}
+
+void clearing() {
+    system("cls");
 }
